9c7ed4678bf68ed13dd978a784739e8cf25de3d5
Andrea Ziani <andrea.ziani@studio.unibo.it>
2019-08-16 17:12:19 +0200

    1.6 string compression with StringBuilder

--- arrays&strings/1.6_stringcompression/main.cpp
+++ arrays&strings/1.6_stringcompression/main.cpp
@@ -3,7 +3,41 @@
 
 using namespace std;
 
+class StringBuilder {
+    private:
+        int length;
+        int curr;
+        char* s;
+
+    public: 
+        StringBuilder(int finalLength){
+            length = finalLength;
+            s = new char[finalLength];
+            curr = 0;
+        }
+
+        void addChar(char c){
+            s[curr] = c;
+            curr++;
+            if(curr == length) {
+                char* tmp = new char[length];
+                for(int i = 0; i < length; i++){
+                    tmp[i] = s[i];
+                }
+                length *=2;
+                for(int i = 0; i < length/2; i++){
+                    s[i] = tmp[i];
+                }
+            }
+        }
+
+        string toString(){
+            return s;
+        }
+};
+
 /* Simple solution: count chars ad add to a new string */
+// ! To improve our solution we can use the StringBuilder class that helps us to build our string without using string concatenation !
 string stringCompression(string s){ // Time compl -> O(n^2) : space Compl -> O(n)
     if(s.size == 0) return s;
     string res = "";
