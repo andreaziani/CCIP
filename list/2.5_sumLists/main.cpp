7e587884ddae946fda0a8bd5235d5d6af6993e19
Andrea Ziani <andrea.ziani@studio.unibo.it>
2019-08-21 14:05:14 +0200

    2.4 sumLists rev order

---
+++ list/2.5_sumLists/main.cpp
@@ -0,0 +1,67 @@
+/*You have 2 numbers represented by a linked list, where each node contains a single digit, stored in reverse order. 
+write a function that adds the two numbers and returns the sum as a linked list. */
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+
+class Node {
+    public: int val; // this is not a good OOP practise but for this ex doesn't matter
+    public: Node* next;
+    
+    public: Node(int v){
+        val = v;
+        next = NULL;
+    }
+
+    public: void push_back(int v){
+        Node* tmp = this;
+        while(tmp->next != NULL){
+            tmp = tmp->next;
+        }
+        tmp->next = new Node(v);
+    }
+
+    public: void printlist(){
+        Node* tmp = this;
+        while(tmp != NULL){
+            cout << tmp->val << " ";
+            tmp = tmp->next;
+        }
+        cout << endl;
+    }
+};
+
+Node* sumLists(Node* l1, Node* l2){
+    Node* res = NULL;
+    Node* tmp = res;
+    int r = 0;
+    int q = 0;
+    while(l1 != NULL){
+        if(l2 != NULL){
+            q = (l1->val + l2->val + r) % 10;
+            r = (l1->val + l2->val + r) / 10;
+            l2 = l2->next;
+        } else{
+            q = (l1->val + r) % 10;
+            r = (l1->val + r) / 10;
+        }
+        if(tmp == NULL) res = tmp = new Node(q);
+        else {
+            tmp->next = new Node(q);
+            tmp = tmp->next;
+        }
+        l1 = l1->next;
+    }
+    return res;
+}
+
+int main(){
+    Node l1 = Node(9);
+    l1.push_back(1);
+    l1.push_back(1);
+    Node l2 = Node(2);
+    l2.push_back(1);
+
+    sumLists(&l1, &l2)->printlist();
+
+}
